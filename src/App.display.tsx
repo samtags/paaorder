6a6e28d52db09537733b674ac909c1d36a2e1af3
Sam <sam.tags@protonmail.ch>
2024-01-27 19:19:56 +0800

    feat: add native-base

--- src/App.display.tsx
+++ src/App.display.tsx
@@ -1,5 +1,10 @@
 import Navigator from '@/services/navigator/Navigator';
+import {NativeBaseProvider} from 'native-base';
 
 export default function App(): React.JSX.Element {
-  return <Navigator />;
+  return (
+    <NativeBaseProvider>
+      <Navigator />
+    </NativeBaseProvider>
+  );
 }
