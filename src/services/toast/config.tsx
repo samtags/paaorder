9fad9dd5ce291be9cff9096f1fc3655b9c5729a9
Sam <sam.tags@protonmail.ch>
2024-01-28 07:51:35 +0800

    style: toast width

--- src/services/toast/config.tsx
+++ src/services/toast/config.tsx
@@ -10,7 +10,23 @@ export default {
   success: (props: Obj) => (
     <BaseToast
       {...props}
-      style={{borderLeftColor: '#00a4e0', width: '90%', margin: 16}}
+      style={{borderLeftColor: '#00a4e0', width: '93%', margin: 16}}
+      contentContainerStyle={{paddingHorizontal: 15}}
+      text1Style={{
+        fontSize: 16,
+        fontWeight: '700',
+      }}
+      text2Style={{
+        fontSize: 13,
+        fontWeight: '400',
+      }}
+    />
+  ),
+
+  error: (props: Obj) => (
+    <BaseToast
+      {...props}
+      style={{borderLeftColor: '#b91c1c', width: '93%', margin: 16}}
       contentContainerStyle={{paddingHorizontal: 15}}
       text1Style={{
         fontSize: 16,
