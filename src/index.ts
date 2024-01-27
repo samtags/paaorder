121b09617500fdb4209b5a23a75c648ae59cafbe
samtags <sam.tags@protonmail.ch>
2024-01-27 18:37:20 +0800

    feat(app): expose context to outside bit component

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
+import {createContext} from 'react';
 import createComponent from '@/services/bit';
 import Display from './App.display';
 import Actions from './App.actions';
 import State from './App.state';
+
+export const ReactContext = createContext({} as unknown);
 
 export default createComponent({
   name: 'App',
   Display,
   Actions,
   State,
+  ReactContext, // required if want to access props outside its bit context.
 });
 
 export * from './App.state';
+export type * from './types';
