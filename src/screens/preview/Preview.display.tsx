c561138199ab3ef1d275f1afd445e1eeeb5e6f07
Sam <sam.tags@protonmail.ch>
2024-01-31 03:39:50 +0800

    feat: remove unnecessary uis

--- src/screens/preview/Preview.display.tsx
+++ src/screens/preview/Preview.display.tsx
@@ -1,19 +1,14 @@
-import {Box, VStack, HStack, Center, Text as Span} from 'native-base';
-import {SafeAreaView, TouchableOpacity} from 'react-native';
+import {Box, VStack, HStack, Text as Span} from 'native-base';
+import {SafeAreaView} from 'react-native';
 import {Customer} from '@/services/api/getCustomers';
 import {Order as IOrder} from '@/services/api/getOrders';
 import moment from 'moment';
-import {useActions, useProps} from '@/services/bit';
-import {Methods} from './Preview.actions';
+import {useProps} from '@/services/bit';
 import amount from '@/services/utils/formatAmount';
-import Optional from '@/components/optional';
 
 export default function Order() {
-  const actions = useActions<Methods>();
-
   const order = useProps<IOrder | undefined>('order');
   const customer = useProps<Customer | undefined>('customer');
-  const countdownTimer = useProps<string>('countdownTimer');
 
   return (
     <SafeAreaView style={{height: '100%'}}>
@@ -52,44 +47,6 @@ export default function Order() {
             </VStack>
           </VStack>
         </VStack>
-
-        <Optional condition={countdownTimer !== '00'}>
-          <HStack bgColor="white" p={4} mt={1} justifyContent="space-between">
-            <Span fontSize="md" fontWeight="medium">
-              Läuft ab in
-            </Span>
-            <Span fontWeight="black" color="red.800" fontSize="md">
-              00:{countdownTimer}
-            </Span>
-          </HStack>
-        </Optional>
-
-        <Box>
-          <VStack bgColor="white" p={4} mt={1}>
-            <HStack justifyContent="space-between">
-              <VStack>
-                <Span fontSize="md" fontWeight="medium">
-                  Zwischensumme
-                </Span>
-                <Span color="gray.400">Inklusive Steuern (0.00)</Span>
-              </VStack>
-              <Span fontSize="md" fontWeight="bold">
-                {amount.format(0)}
-              </Span>
-            </HStack>
-          </VStack>
-
-          <VStack bgColor="white" p={4}>
-            <HStack justifyContent="space-between" alignItems="center">
-              <Span fontSize="md" fontWeight="medium">
-                Gesamt
-              </Span>
-              <Span fontSize="md" fontWeight="bold">
-                {amount.format(Number(order?.totalPrice))}
-              </Span>
-            </HStack>
-          </VStack>
-        </Box>
       </VStack>
     </SafeAreaView>
   );
--- src/screens/preview/Preview.state.ts
+++ src/screens/preview/Preview.state.ts
@@ -4,5 +4,4 @@ import {Order} from '@/services/api/getOrders';
 export default {
   order: undefined as Order | undefined,
   customer: undefined as Customer | undefined,
-  countdownTimer: '00',
 };
